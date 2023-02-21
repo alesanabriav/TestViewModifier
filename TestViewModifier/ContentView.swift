14c54b4a2633f703e6cd20878515422b8f94b06a
Alejandro Sanabria <alesanabriav@gmail.com>
2023-02-21 16:25:45 -0500

    [add] example of ViewModifier

--- TestViewModifier/ContentView.swift
+++ TestViewModifier/ContentView.swift
@@ -7,15 +7,44 @@
 
 import SwiftUI
 
+struct TextButtonViewModifier : ViewModifier {
+    
+    var bgColor: Color = .blue
+    var height: CGFloat = 48
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .foregroundColor(.white)
+            .frame(height: height)
+            .frame(maxWidth: .infinity)
+            .background(bgColor)
+            .cornerRadius(8)
+            .shadow(radius: 8)
+            .padding()
+    }
+}
+
+extension View {
+    
+    func textButtonStyle(bgColor: Color = .blue, height: CGFloat = 48) -> some View {
+        modifier(TextButtonViewModifier(bgColor: bgColor, height: height))
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Text Button")
+                .textButtonStyle(bgColor: .cyan)
+            
+            Text("Text Button 2")
+                .modifier(TextButtonViewModifier(bgColor: .red))
+            
+            Text("Text Button 3")
+                .modifier(TextButtonViewModifier(height: 58))
+            
         }
-        .padding()
     }
 }
 
