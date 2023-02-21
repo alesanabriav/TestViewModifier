407e7abc41feaa941c844338a8e32bad37939e0b
Alejandro Sanabria <alesanabriav@gmail.com>
2023-02-21 16:13:25 -0500

    Initial Commit

---
+++ TestViewModifier/TestViewModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestViewModifierApp.swift
+//  TestViewModifier
+//
+//  Created by Alejandro Sanabria on 21/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TestViewModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
